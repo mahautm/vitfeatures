135ccae901176ec1513ca7dc7eb9242bd68f88c3
mahautm <mmahaut@ensc.fr>
2022-02-25 19:36:46 +0100

    cuda

--- train.py
+++ train.py
@@ -28,8 +28,10 @@ def train_model(model, train_data_loader, n_epochs=10, label_model=None, verbose
         running_loss = 0.0
         for i, data in enumerate(train_data_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
-            inputs, _labels = data.to(device)
+            inputs, _labels = data
+            inputs = inputs.to(device)         
             labels = _labels if label_model is None else label_model(inputs)
+            labels = labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
 
