427d8f4bdd982c601661506d81acfaf396026243
mahautm <mmahaut@ensc.fr>
2022-02-25 18:53:01 +0100

    debug

--- train.py
+++ train.py
@@ -29,6 +29,8 @@ def train_model(model, train_data_loader, n_epochs=10, label_model=None, verbose
         for i, data in enumerate(train_data_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, _labels = data
+            print(inputs)
+            print(len(inputs))
             labels = _labels if label_model is None else label_model(inputs)
             # zero the parameter gradients
             optimizer.zero_grad()
