4f6ffb2ca536b7b7e6dac6e8715288208af1dbb5
mahautm <mmahaut@ensc.fr>
2022-03-11 12:47:40 +0100

    rsa

---
+++ rsa.py
@@ -0,0 +1,46 @@
+# Usage:
+# python ./compute_rsa_on_resnet_outputs.py \
+# /private/home/rdessi/interactions_for_marco/latest_interaction_for_neurips_all_fields_valid
+import sys
+import torch
+import numpy as np
+from sklearn import metrics
+
+# from scipy.stats import spearmanr
+from scipy.stats import pearsonr
+
+device = "cuda"
+interaction_path = sys.argv[1]
+print(f"loading {interaction_path} and reading in data")
+interaction = torch.load(interaction_path)
+sender_res = interaction.aux["resnet_output_sender"].numpy()
+receiver_res = interaction.aux["resnet_output_recv"].numpy()
+
+print("computing pairwise cosines")
+sender_cos = metrics.pairwise.cosine_similarity(sender_res)
+receiver_cos = metrics.pairwise.cosine_similarity(receiver_res)
+
+print("extracting uppper triangular matrices")
+sender_upper_tri = sender_cos[np.triu_indices(sender_cos.shape[0], k=1)]
+receiver_upper_tri = receiver_cos[np.triu_indices(receiver_cos.shape[0], k=1)]
+print("computing Pearson and Spearman correlations")
+print(f"Pearson correlation: {pearsonr(sender_upper_tri,receiver_upper_tri)[0]}")
+# print(f"Spearman correlation: {spearmanr(sender_upper_tri,receiver_upper_tri).correlation}")
+
+
+def compute_model_rsa(train_data_loader, model1, model2):
+    # print("reading data")
+    images = [data for data in train_data_loader]
+    model1 = model1.to(device)
+    model2 = model2.to(device)
+    # print("feature extraction")
+    features1 = model1(images.to(device)).to("cpu")
+    features2 = model2(images.to(device)).to("cpu")
+    # print("computing pairwise cosines")
+    f1_cos = metrics.pairwise.cosine_similarity(features1)
+    f2_cos = metrics.pairwise.cosine_similarity(features2)
+    # print("extracting uppper triangular matrices")
+    f1_upper_tri = sender_cos[np.triu_indices(f1_cos.shape[0], k=1)]
+    f2_upper_tri = receiver_cos[np.triu_indices(f2_cos.shape[0], k=1)]
+    # print("computing Pearson and Spearman correlations")
+    return f1_cos, f2_cos, pearsonr(f1_upper_tri, f2_upper_tri)[0]
--- v2.py
+++ v2.py
@@ -9,6 +9,7 @@ from data import get_dataloader
 from archs import initialize_vision_module, behead_freeze
 from train import train_model
 from test import test_models
+from rsa import compute_model_rsa
 import torch
 from torch import nn
 from torch.nn import Sequential
@@ -19,23 +20,11 @@ output_path = "/mnt/efs/fs1/logs/vitfeatures"
 device = "cuda"
 
 
-def features(model_name_1="vit", model_name_2="vgg11"):
-    # get data
-    test_data_loader, train_data_loader = get_dataloader(
-        dataset_dir="",  # not required for cifar100
-        dataset_name="cifar100",
-        image_size=384,
-        batch_size=1,
-        num_workers=2,
-        seed=123,
-        return_original_image=False,
-    )
-    """
-    TODO : n_epochs is implicitly set at 10 during training, think on that
-    TODO : validation must be added during training, as well as better logging
-    """
-    # get and train models
-
+def get_models(
+    train_data_loader=None,
+    model_name_1=None,
+    model_name_2=None,
+):
     model1 = initialize_vision_module(model_name_1)
     model2 = initialize_vision_module(model_name_2)
     optimiser1 = optim.Adam(model1.parameters(), lr=0.0001)
@@ -60,6 +49,28 @@ def features(model_name_1="vit", model_name_2="vgg11"):
     output_path.mkdir(exist_ok=True, parents=True)
     torch.save(model1, output_path / model_name_1)
     torch.save(model2, output_path / model_name_2)
+
+    return model1, model2
+
+
+def features(model_name_1="vit", model_name_2="vgg11"):
+    """
+    TODO : n_epochs is implicitly set at 10 during training, think on that
+    TODO : validation must be added during training, as well as better logging
+    """
+    # get data
+    test_data_loader, train_data_loader = get_dataloader(
+        dataset_dir="",  # not required for cifar100
+        dataset_name="cifar100",
+        image_size=128,
+        batch_size=16,
+        num_workers=2,
+        seed=123,
+        return_original_image=False,
+    )
+
+    # get and train models
+    model1, model2 = get_models(model_name_1, model_name_2)
 
     # get features from one model to be linked by a linear layer to the other's classification layer
     feature_extractor_1, classifier_1, n_features1 = behead_freeze(model1, model_name_1)
@@ -95,7 +106,25 @@ def features(model_name_1="vit", model_name_2="vgg11"):
     pass
 
 
-def full_experiment(model_name_1="vit", model_name_2="vgg11"):
+def rsa_check(model1_path, model2_path):
+    # load trained models
+    _, train_data_loader = get_dataloader(
+        dataset_dir="",  # not required for cifar100
+        dataset_name="cifar100",
+        image_size=384,
+        batch_size=1,
+        num_workers=2,
+        seed=123,
+        return_original_image=False,
+    )
+    model1, model2 = torch.load(model1_path), torch.load(model2_path)
+    f1_cos, f2_cos, pearsonr = compute_model_rsa(train_data_loader, model1, model2)
+    print(f1_cos, f2_cos, pearsonr)
+    # store data / make some kind of graph
+    pass
+
+
+def reproduce_paper(model_name_1="vit", model_name_2="vgg11"):
     # get data
     test_data_loader, train_data_loader = get_dataloader(
         dataset_dir="",  # not required for cifar100
