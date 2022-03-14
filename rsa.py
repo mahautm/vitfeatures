746ddeee23438dc9d33072c65c2afffd160a2342
mahautm <mmahaut@ensc.fr>
2022-03-14 12:24:43 +0100

    pretrained

--- rsa.py
+++ rsa.py
@@ -7,25 +7,25 @@ import numpy as np
 from sklearn import metrics
 
 # from scipy.stats import spearmanr
-from scipy.stats import pearsonr
+# from scipy.stats import pearsonr
 
-device = "cuda"
-interaction_path = sys.argv[1]
-print(f"loading {interaction_path} and reading in data")
-interaction = torch.load(interaction_path)
-sender_res = interaction.aux["resnet_output_sender"].numpy()
-receiver_res = interaction.aux["resnet_output_recv"].numpy()
+# device = "cuda"
+# interaction_path = sys.argv[1]
+# print(f"loading {interaction_path} and reading in data")
+# interaction = torch.load(interaction_path)
+# sender_res = interaction.aux["resnet_output_sender"].numpy()
+# receiver_res = interaction.aux["resnet_output_recv"].numpy()
 
-print("computing pairwise cosines")
-sender_cos = metrics.pairwise.cosine_similarity(sender_res)
-receiver_cos = metrics.pairwise.cosine_similarity(receiver_res)
+# print("computing pairwise cosines")
+# sender_cos = metrics.pairwise.cosine_similarity(sender_res)
+# receiver_cos = metrics.pairwise.cosine_similarity(receiver_res)
 
-print("extracting uppper triangular matrices")
-sender_upper_tri = sender_cos[np.triu_indices(sender_cos.shape[0], k=1)]
-receiver_upper_tri = receiver_cos[np.triu_indices(receiver_cos.shape[0], k=1)]
-print("computing Pearson and Spearman correlations")
-print(f"Pearson correlation: {pearsonr(sender_upper_tri,receiver_upper_tri)[0]}")
-# print(f"Spearman correlation: {spearmanr(sender_upper_tri,receiver_upper_tri).correlation}")
+# print("extracting uppper triangular matrices")
+# sender_upper_tri = sender_cos[np.triu_indices(sender_cos.shape[0], k=1)]
+# receiver_upper_tri = receiver_cos[np.triu_indices(receiver_cos.shape[0], k=1)]
+# print("computing Pearson and Spearman correlations")
+# print(f"Pearson correlation: {pearsonr(sender_upper_tri,receiver_upper_tri)[0]}")
+# # print(f"Spearman correlation: {spearmanr(sender_upper_tri,receiver_upper_tri).correlation}")
 
 
 def compute_model_rsa(train_data_loader, model1, model2):
