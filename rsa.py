b1da8c2db84985796d0127bb3005c22f85c7589b
mahautm <mmahaut@ensc.fr>
2022-03-15 18:32:04 +0100

    scale_gpu_memory

--- rsa.py
+++ rsa.py
@@ -31,14 +31,15 @@ device = "cuda"
 
 def compute_model_rsa(train_data_loader, model1, model2, n_images=10000):
     # print("reading data")
-    features = torch.Tensor([[],[]])
-    for model_number, model in enumerate(model1, model2) :
+    features = torch.Tensor([[], []])
+    for model_number, model in enumerate([model1, model2]):
         model = model.to(device)
-        for i in range(n_images//train_data_loader.batch_size):
+        for i in range(n_images // train_data_loader.batch_size):
             images, _ = next(iter(train_data_loader))
-            features[model_number] = torch.cat(features[model_number],model(images.to(device)).to("cpu"))
+            features[model_number] = torch.cat(
+                features[model_number], model(images.to(device)).to("cpu")
+            )
 
-    
     model2 = model2.to(device)
     # print("feature extraction")
     features2 = model2(torch.Tensor(images).to(device)).to("cpu")
